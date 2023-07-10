eeb53791894fa27ab5c1c64b2a912cee907ba109
kojikojiXX <kouji.nohanoha@gmail.com>
2023-07-10 17:03:12 +0900

    feat: useGetRequestの作成

---
+++ app/providers.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
