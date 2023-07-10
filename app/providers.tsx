0c8ff0711d6428d70ff4c8741dcb61f41b876c6f
kojikojiXX <kouji.nohanoha@gmail.com>
2023-07-10 17:13:00 +0900

    fix: 不用部分の削除とメタデータの作成

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient({
