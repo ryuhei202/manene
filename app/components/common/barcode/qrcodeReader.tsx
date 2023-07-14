0b5afdd4df5b693535cf213be052d3020702e7ce
kojikojiXX <kouji.nohanoha@gmail.com>
2023-07-14 14:58:07 +0900

    feat: barcodeInput.tsxの完成

--- app/components/common/barcode/qrcodeReader.tsx
+++ app/components/common/barcode/qrcodeReader.tsx
@@ -1,29 +1,52 @@
 "use client";
+import { Button } from "@mui/material";
 import { useState } from "react";
 import { QrReader } from "react-qr-reader";
+import KeyboardAltIcon from "@mui/icons-material/KeyboardAlt";
+import BarcodeInput from "./barcodeInput";
 
-export default function QrCodeReader() {
-  const [data, setData] = useState<object>();
+type TProps = {
+  onScan: (id: number) => void;
+};
 
-  console.log(data);
+export default function QrCodeReader({ onScan }: TProps) {
+  const [isOpenBarcodeInput, setIsOpenBarcodeInput] = useState(false);
 
+  const handleClickSwitchModal = () => {
+    setIsOpenBarcodeInput(!isOpenBarcodeInput);
+  };
+
+  console.log(isOpenBarcodeInput);
   return (
     <>
-      <QrReader
-        constraints={{ facingMode: "environment" }}
-        onResult={(result, error) => {
-          if (result) {
-            setData(result);
-          }
+      {!isOpenBarcodeInput ? (
+        <>
+          <QrReader
+            constraints={{ facingMode: "user" }}
+            onResult={(result, error) => {
+              if (result) {
+                onScan(parseInt(result.getText()));
+                console.log(result.text);
+              }
 
-          if (error) {
-            // eslint-disable-next-line no-console
-            console.info(error);
-          }
-        }}
-        containerStyle={{ width: "100%" }}
-      />
-      <p>{data?.text}</p>
+              if (error) {
+                // eslint-disable-next-line no-console
+                console.info(error);
+              }
+            }}
+            containerStyle={{ width: "100%" }}
+          />
+          {/* <p>{data?.text}</p> */}
+          <Button onClick={handleClickSwitchModal}>
+            <KeyboardAltIcon />
+          </Button>
+        </>
+      ) : (
+        <BarcodeInput
+          onClickSetId={onScan}
+          onClickCloseBarcodeModal={handleClickSwitchModal}
+        />
+      )}
     </>
   );
 }
