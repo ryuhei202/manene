9600a9ae1a90b03b07ceb442ba7166e6b8a3d306
kojikojiXX <kouji.nohanoha@gmail.com>
2023-07-15 15:44:52 +0900

    feat: データをフェッチするコンポーネントを作成

---
+++ app/components/coorde-pick/chart-item-fetcher.tsx
@@ -0,0 +1,28 @@
+import useCoordePicksIndex from "@/app/api/coorde_pick/useCoordePicksIndex";
+import CoordePickList from "./coorde-pick-list";
+import ErrorPage from "../common/pages/error-page";
+import LoadingPage from "../common/pages/loading-page";
+
+type TProps = {
+  tChartId: number;
+  onClickBackQrReader: () => void;
+};
+
+export default function ChartItemFetcher({
+  tChartId,
+  onClickBackQrReader,
+}: TProps) {
+  const {
+    data: tChartItemsData,
+    error: tChartItemsError,
+    isLoading,
+  } = useCoordePicksIndex({ tChartId });
+  if (tChartItemsError) return <ErrorPage massage={tChartItemsError.message} />;
+  if (!tChartItemsData || isLoading) return <LoadingPage />;
+  return (
+    <CoordePickList
+      tChartItems={tChartItemsData}
+      onClickBackQrReader={onClickBackQrReader}
+    />
+  );
+}
