03f816161f570ba30a635c617447dc7add83e010
kojikojiXX <kouji.nohanoha@gmail.com>
2023-07-20 12:58:32 +0900

    refactor: 三項演算子の削除

--- app/components/common/barcode/qrcodeReader.tsx
+++ app/components/common/barcode/qrcodeReader.tsx
@@ -3,7 +3,7 @@ import { Box, Button } from "@mui/material";
 import { useState } from "react";
 import { QrReader } from "react-qr-reader";
 import KeyboardAltOutlinedIcon from "@mui/icons-material/KeyboardAltOutlined";
-import BarcodeInput from "./barcodeInput";
+import BarcodeInputDialog from "./barcode-input-dialog";
 
 type TProps = {
   onScan: (id: number) => void;
@@ -17,61 +17,58 @@ export default function QrCodeReader({ onScan }: TProps) {
 
   return (
     <>
-      {!isOpenBarcodeInput ? (
-        <>
-          <Box
-            sx={{
-              height: "100vh",
-              width: "100vw",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <QrReader
-              constraints={{ facingMode: "environment" }}
-              onResult={(result, error) => {
-                if (result) {
-                  onScan(parseInt(result.getText()));
-                }
+      <>
+        <Box
+          sx={{
+            height: "100vh",
+            width: "100vw",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <QrReader
+            constraints={{ facingMode: "environment" }}
+            onResult={(result, error) => {
+              if (result) {
+                onScan(parseInt(result.getText()));
+              }
 
-                if (error) {
-                  // eslint-disable-next-line no-console
-                  console.info(error);
-                }
-              }}
-              containerStyle={{
-                width: "100%",
-                position: "absolute",
-                top: "50%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-              }}
-              videoStyle={{ width: "100%" }}
-            />
-          </Box>
-          <Button
-            onClick={handleClickSwitchModal}
-            sx={{
-              backgroundColor: "#1976d2",
-              position: "absolute",
-              bottom: "12vh",
-              right: "12.5vw",
-              width: "40px",
-              height: "40px",
-              minWidth: "40px",
+              if (error) {
+                // eslint-disable-next-line no-console
+                console.info(error);
+              }
             }}
-          >
-            <KeyboardAltOutlinedIcon sx={{ color: "white" }} />
-          </Button>
-        </>
-      ) : (
-        <BarcodeInput
-          onClickSetId={onScan}
-          onClickCloseBarcodeModal={handleClickSwitchModal}
-          isOpen={isOpenBarcodeInput}
-        />
-      )}
+            containerStyle={{
+              width: "100%",
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+            videoStyle={{ width: "100%" }}
+          />
+        </Box>
+        <Button
+          onClick={handleClickSwitchModal}
+          sx={{
+            backgroundColor: "#1976d2",
+            position: "absolute",
+            bottom: "12vh",
+            right: "12.5vw",
+            width: "40px",
+            height: "40px",
+            minWidth: "40px",
+          }}
+        >
+          <KeyboardAltOutlinedIcon sx={{ color: "white" }} />
+        </Button>
+      </>
+      <BarcodeInputDialog
+        onClickSetId={onScan}
+        onClose={handleClickSwitchModal}
+        isOpen={isOpenBarcodeInput}
+      />
     </>
   );
 }
