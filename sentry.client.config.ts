7d82877f9e042dc31abea23d8d7c1b88703926a8
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-07-20 18:23:55 +0900

    Revert "feat: test: vercelの環境変数を読み込むようにする"
    This reverts commit 5a8547a7ff1656ae9a0bb4108d0ffbd209e6fe1d.

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,7 +2,7 @@ import * as Sentry from "@sentry/nextjs";
 
 if (process.env.NODE_ENV === "production") {
   Sentry.init({
-    dsn: process.env.SENTRY_DSN,
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
     tracesSampleRate: 0.3,
     environment: process.env.NODE_ENV,
   });
