a23f272f7e173af196aa17a47b2873c4955fd1a8
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-07-28 13:42:42 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          test1&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
         <div>
