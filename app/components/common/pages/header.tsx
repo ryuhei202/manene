82cb8e207c6995143c6822dfaededd6696c1a101
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-08-11 17:37:26 +0900

    fix: headerの影を無くし、タップするとトップに戻るよう修正

---
+++ app/components/common/pages/header.tsx
@@ -0,0 +1,28 @@
+import { AppBar, Button } from "@mui/material";
+import Link from "next/link";
+
+type TProps = {
+  title: string;
+};
+
+export default function Header({ title }: TProps) {
+  return (
+    <AppBar position="static" elevation={0}>
+      <Link href={"/"} passHref>
+        <Button
+          style={{
+            color: "white",
+            marginTop: "8px",
+            marginBottom: "8px",
+            marginLeft: "12px",
+            marginRight: "12px",
+            fontSize: "1.25rem",
+            fontWeight: 500,
+          }}
+        >
+          {title}
+        </Button>
+      </Link>
+    </AppBar>
+  );
+}
