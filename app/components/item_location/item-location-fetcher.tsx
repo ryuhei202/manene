e9f7006377526f8fa5f0258010900ef712adfe5f
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-08-11 17:31:55 +0900

    feat: 読み取ったitemIdからitemの情報を取得するfetcherを作成

---
+++ app/components/item_location/item-location-fetcher.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import useItemLocationsItemScan, {
+  TItemLocationsItemScanResponse,
+} from "@/app/api/item-location/useItemLocationsItemScan";
+import { useEffect } from "react";
+import ErrorDialog from "../common/dialog/error-dialog";
+import LoadingDialog from "../common/dialog/loading-dialog";
+
+type TProps = {
+  itemId: number;
+  onSetItem: (data: TItemLocationsItemScanResponse) => void;
+  onUnSetItemId: () => void;
+};
+
+export default function ItemLocationFetcher({
+  itemId,
+  onSetItem,
+  onUnSetItemId,
+}: TProps) {
+  const { data, isLoading, error } = useItemLocationsItemScan({
+    itemId: itemId,
+  });
+
+  useEffect(() => {
+    if (data) {
+      onSetItem(data);
+      onUnSetItemId();
+    }
+  }, [data, onSetItem, onUnSetItemId]);
+
+  if (error) return <ErrorDialog message={error.message} />;
+  if (isLoading) return <LoadingDialog />;
+  return <></>;
+}
