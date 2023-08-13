03c050f170c264ebc34351f01564b35a820ef8ec
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-08-13 13:42:21 +0900

    feat: stocktakings/createへapiコールするためのフックを作成

---
+++ app/api/stocktaking/useStocktakingsCreate.ts
@@ -0,0 +1,10 @@
+import { usePostRequest } from "../usePostRequest";
+import { TStocktakingsCurrentResponse as TStocktakingsCreateResponse } from "./getStocktakingsCurrent";
+
+export default function useStocktakingsCreate() {
+  const { mutate, error, isLoading } = usePostRequest<
+    undefined,
+    TStocktakingsCreateResponse
+  >({ path: "stocktakings/create" });
+  return { mutate, error, isLoading };
+}
