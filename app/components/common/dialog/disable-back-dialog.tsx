d22f06466e778cb42feaa3fc85f064c84d194d04
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-08-21 13:57:40 +0900

    fix: 戻るボタンを考慮したダイアログコンポーネントを作成

---
+++ app/components/common/dialog/disable-back-dialog.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { Dialog } from "@mui/material";
+import { usePathname } from "next/navigation";
+import React, { useEffect } from "react";
+
+type TProps = {
+  open: boolean;
+  altCallback: () => void;
+  children: React.ReactNode;
+};
+
+export default function DisableBackDialog({
+  open,
+  altCallback,
+  children,
+}: TProps) {
+  const pathname = usePathname();
+  useEffect(() => {
+    if (open) {
+      history.pushState("", "", pathname);
+      addEventListener("popstate", altCallback);
+      return () => {
+        removeEventListener("popstate", altCallback);
+      };
+    }
+  }, [open, altCallback, pathname]);
+  return (
+    <Dialog open={open} fullWidth={}>
+      {children}
+    </Dialog>
+  );
+}
