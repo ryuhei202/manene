afce5651f1e6cdb1cc420d4c127b233a2fcf7d83
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-08-28 16:32:35 +0900

    fix: 型をスネークケースからキャメルケースに修正

--- app/api/inspection-groups/getInspectionGroupsIndex.ts
+++ app/api/inspection-groups/getInspectionGroupsIndex.ts
@@ -3,13 +3,13 @@ import getNoCacheData from "../getNoCacheData";
 export type TInspectionGroupResponse = {
   inspection_groups: {
     id: number;
-    group_no: number;
-    registration_start_time: string;
-    registration_end_time?: string;
-    inspection_end_time?: string;
-    chart_count: number;
-    washing_item_count: number;
-    purchase_item_count: number;
+    groupNo: number;
+    registrationStartTime: string;
+    registrationEndTime?: string;
+    inspectionEndTime?: string;
+    chartCount: number;
+    washingItemCount: number;
+    purchaseItemCount: number;
   }[];
 };
 
