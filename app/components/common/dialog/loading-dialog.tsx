e2bd9eae359e4ed7336ae218c3cfd661883381a2
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-08-31 10:57:15 +0900

    fix: Backdropを消去

--- app/components/common/dialog/loading-dialog.tsx
+++ app/components/common/dialog/loading-dialog.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Backdrop, Box, CircularProgress, Dialog } from "@mui/material";
+import { Box, CircularProgress, Dialog } from "@mui/material";
 
 type TProps = {
   isOpen: boolean;
@@ -7,21 +7,16 @@ type TProps = {
 
 export default function LoadingDialog({ isOpen }: TProps) {
   return (
-    <Backdrop
-      sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-      open={isOpen}
-    >
-      <Dialog open={isOpen}>
-        <Box
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          width="100px"
-          height="100px"
-        >
-          <CircularProgress color="primary" />
-        </Box>
-      </Dialog>
-    </Backdrop>
+    <Dialog open={isOpen}>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        width="100px"
+        height="100px"
+      >
+        <CircularProgress color="primary" />
+      </Box>
+    </Dialog>
   );
 }
