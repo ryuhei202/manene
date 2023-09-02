82e0263ee5a3d0403bb75f5a9ed2f48ec2348ba9
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-09-02 13:02:43 +0900

    fix: ItemInfoCardの修正。ステイタスによってカードの右端に出るコンテンツを変更できるようにする

--- app/components/common/Item/item-info-card.tsx
+++ app/components/common/Item/item-info-card.tsx
@@ -1,39 +1,110 @@
 "use client";
-import { TCoordePicksIndexResponse } from "@/app/api/coorde_pick/useCoordePicksIndex";
-import { Box, Typography } from "@mui/material";
-
-type TItemInfo = Omit<TCoordePicksIndexResponse, "itemImageUrl" | "isPicked">;
+import { TItemInfo } from "@/app/api/before-inspections/useBeforeInspectionsCreate";
+import { Box, Button, Typography } from "@mui/material";
+import React, { useMemo } from "react";
 
 type TProps = {
-  itemInfo: TItemInfo;
+  itemInfo: TItemInfo & {
+    mLocationName?: string;
+  };
+  onClick?: () => void;
+  isLoading?: boolean;
+  isPurchased?: boolean;
+  inspectionStatus?: number;
 };
 
-export default function ItemInfoCard({ itemInfo }: TProps) {
+function InspectionStatusText({ children }: { children: React.ReactNode }) {
+  return (
+    <Typography color="warning.dark" variant="body2" marginRight={2}>
+      {children}
+    </Typography>
+  );
+}
+
+export const INSPECTION_STATUS = {
+  RETURNED: 0,
+  MISPLACED: 1,
+  INSPECTED: 2,
+  WASHING: 3,
+  PURCHASE_REQUEST: 4,
+  PURCHASE_CANDIDATE: 5,
+} as const;
+
+export default function ItemInfoCard({
+  itemInfo,
+  onClick,
+  isLoading,
+  isPurchased,
+  inspectionStatus,
+}: TProps) {
+  const statusView = useMemo(() => {
+    return {
+      [INSPECTION_STATUS.RETURNED]: (
+        <Button
+          onClick={onClick}
+          disabled={isLoading}
+          variant="contained"
+          sx={{ marginRight: "15px" }}
+        >
+          入れ忘れ
+        </Button>
+      ),
+      [INSPECTION_STATUS.MISPLACED]: (
+        <InspectionStatusText>入れ忘れ登録済み</InspectionStatusText>
+      ),
+      [INSPECTION_STATUS.INSPECTED]: (
+        <InspectionStatusText>検品済み</InspectionStatusText>
+      ),
+      [INSPECTION_STATUS.WASHING]: (
+        <InspectionStatusText>汚れ確認中</InspectionStatusText>
+      ),
+      [INSPECTION_STATUS.PURCHASE_REQUEST]: (
+        <InspectionStatusText>買取依頼済み</InspectionStatusText>
+      ),
+      [INSPECTION_STATUS.PURCHASE_CANDIDATE]: (
+        <InspectionStatusText>買取候補</InspectionStatusText>
+      ),
+    }[inspectionStatus as number];
+  }, [inspectionStatus, onClick, isLoading]);
+
   return (
     <>
-      <Typography component="div" display={"flex"} fontSize={"h5.fontSize"}>
-        <Box>棚名</Box>
-        <Box sx={{ mx: 1 }}>:</Box>
-        <Box>{itemInfo.mLocationName}</Box>
-      </Typography>
-      <Typography
-        component="div"
-        sx={{
-          display: "flex",
-          justifyContent: "flex-start",
-          fontSize: 17,
-        }}
-      >
-        <Box mr={1.5}>{itemInfo.id}</Box>
-        <Box>{itemInfo.mBrandName}</Box>
-      </Typography>
-      <Typography component="div" display={"flex"} fontSize={17}>
-        <Box>{itemInfo.size}</Box>
-        <Box mx={0.5}>/</Box>
-        <Box>{itemInfo.mCateSmallName}</Box>
-        <Box mx={0.5}>/</Box>
-        <Box>{itemInfo.mColorName}</Box>
-      </Typography>
+      {itemInfo.mLocationName && (
+        <Typography component="div" display={"flex"} fontSize={"h5.fontSize"}>
+          <Box>棚名</Box>
+          <Box sx={{ mx: 1 }}>:</Box>
+          <Box>{itemInfo.mLocationName}</Box>
+        </Typography>
+      )}
+      <Box>
+        <Box>
+          <Typography
+            component="div"
+            sx={{
+              display: "flex",
+              justifyContent: "flex-start",
+              fontSize: 17,
+            }}
+          >
+            <Box mr={1.5}>{itemInfo.id}</Box>
+            <Box>{itemInfo.mBrandName}</Box>
+          </Typography>
+          <Typography component="div" display={"flex"} fontSize={17}>
+            <Box>{itemInfo.size}</Box>
+            <Box mx={0.5}>/</Box>
+            <Box>{itemInfo.mCateSmallName}</Box>
+            <Box mx={0.5}>/</Box>
+            <Box>{itemInfo.mColorName}</Box>
+          </Typography>
+        </Box>
+        <Box display="flex" alignItems="center" justifyContent="end">
+          {isPurchased ? (
+            <InspectionStatusText>購入済み</InspectionStatusText>
+          ) : (
+            statusView
+          )}
+        </Box>
+      </Box>
     </>
   );
 }
