ff1a558136097bde4eef4090c0c5c022c7f3d15e
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-09-04 16:24:31 +0900

    feat: [id]のpageとcontainerを作成

---
+++ app/wasted_reason/[id]/page.tsx
@@ -0,0 +1,36 @@
+import getItemsShow from "@/app/api/item_detail/getItemsShow";
+import getWastedReasonIndex from "@/app/api/wasted_reason/getWastedReasonIndex";
+import dynamic from "next/dynamic";
+const WastedReasonContainer = dynamic(
+  () => import("../../components/wasted-reason/wasted-reason-container"),
+  {
+    ssr: false,
+  }
+);
+
+type TProps = {
+  params: {
+    id: number;
+  };
+};
+
+export default async function WastedReasonIdPage({ params }: TProps) {
+  const [itemData, wastedReasonData] = await Promise.all([
+    getItemsShow(params),
+    getWastedReasonIndex(params),
+  ]);
+  return (
+    <WastedReasonContainer
+      itemInfo={{
+        id: itemData.id,
+        itemImageUrl: itemData.itemImageUrl,
+        mBrand: itemData.mBrand.name,
+        size: itemData.size,
+        mCateSmall: itemData.mCateSmall.name,
+        mColor: itemData.mColor.name,
+        status: itemData.itemStatus.name,
+      }}
+      wastedReasons={wastedReasonData.wastedReasons}
+    />
+  );
+}
