80a415f1b2b907edd88dc0a27a0bb322d6828cac
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-09-06 16:55:16 +0900

    feat: InspectionItemsContainerを作成

---
+++ app/inspecting_items/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import InspectionItemsContainer from "../components/inspecting-items/inspecting-items-container";
+
+export default function InspectionItemsPage() {
+  return <InspectionItemsContainer />;
+}
