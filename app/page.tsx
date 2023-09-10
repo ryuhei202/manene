d353c645618fbe39431e61aa1cb4d2e261b85ced
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-09-11 07:37:10 +0900

    feat: pathの追加

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
       <Header title="メニュー" />
       <MenuAccordion title="運営">
         <LinkButton buttonName="アイテムサイズ計測" path="" />
-        <LinkButton buttonName="クリーニングコスト登録" path="" />
+        <LinkButton buttonName="クリーニングコスト登録" path="cleaning_costs" />
       </MenuAccordion>
       <MenuAccordion title="返却検品">
         <LinkButton buttonName="返却検品前登録" path="before_inspection" />
