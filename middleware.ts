841849f0ba30be671acfdead556264b1b15f0728
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-09-14 12:09:26 +0900

    add console.log追加

--- middleware.ts
+++ middleware.ts
@@ -10,9 +10,11 @@ export const allowedIPs: string[] = [
     return VPN ?? "";
   }),
 ];
+
 export function middleware(request: NextRequest) {
   if (process.env.NODE_ENV !== "production") return;
   const clientIP = request.ip || request.headers.get("x-forwarded-host");
+  console.log(clientIP);
   if (!clientIP || !allowedIPs.includes(clientIP)) {
     return NextResponse.redirect(new URL("/not_found", request.url));
   }
