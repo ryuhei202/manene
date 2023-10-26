e4331f7cf811de2a4302ea72660775ee7eaf5630
Haruka <hfujii1013@gmail.com>
2023-10-25 18:48:14 -0700

    fix: ファイル名及び場所の変更

---
+++ app/_api/items/itemsShowResponse.ts
@@ -0,0 +1,74 @@
+export type TItemsShowResponse = {
+  id: number;
+  usedCount: number;
+  itemCode: string;
+  isMarriage: boolean;
+  isElasticBand: boolean;
+  itemStatus: {
+    status: number;
+    name: string;
+  };
+  size: string | null;
+  dropSize: {
+    id: number;
+    name: string;
+  };
+  regDate: string | null;
+  priceTaxIn: number;
+  priceRefTaxIn: number;
+  shoulder: number | null;
+  bust: number | null;
+  waist: number | null;
+  minWaist: number | null;
+  maxWaist: number | null;
+  hip: number | null;
+  lengthArm: number | null;
+  lengthTop: number | null;
+  lengthWaist: number | null;
+  lengthLeg: number | null;
+  roundNeck: number | null;
+  roundLeg: number | null;
+  roundCalf: number | null;
+  itemImageUrl: string;
+  itemOriginalImageUrl: string;
+  leeapSize: string;
+  tStockingOrderId: number;
+  originalSize: number;
+  rank: string;
+  mCateSmall: {
+    id: number;
+    name: string;
+  };
+  mBrand: {
+    id: number;
+    name: string;
+  };
+  mColor: {
+    id: number;
+    name: string;
+  };
+  mSubColor: {
+    id: number;
+    name: string;
+  } | null;
+  mPattern: {
+    id: number;
+    name: string;
+  };
+  mLogo: {
+    id: number;
+    name: string;
+  };
+  tAdmin: {
+    id: number;
+    name: string;
+  };
+  mLocation: {
+    id: number;
+    name: string;
+  } | null;
+};
+
+export type TParams = {
+  id: number;
+};
