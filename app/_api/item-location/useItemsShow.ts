354fe4e8a2970ba52b7f8178438548d835779c2b
Haruka <hfujii1013@gmail.com>
2023-10-25 18:49:49 -0700

    fix: TGetItemsShow→TItemsShowResponse変更

--- app/_api/item-location/useItemsShow.ts
+++ app/_api/item-location/useItemsShow.ts
@@ -1,11 +1,13 @@
-import { TGetItemsShow, TParams } from '../../../types/types';
+import { TItemsShowResponse, TParams } from '../items/itemsShowResponse';
 import { useGetRequest } from '../useGetRequest';
 
 export default function useItemsShow(params: TParams) {
   const { id } = params;
-  const { data, error, isLoading } = useGetRequest<TGetItemsShow, TParams>({
-    path: `items/${id}`,
-    params,
-  });
+  const { data, error, isLoading } = useGetRequest<TItemsShowResponse, TParams>(
+    {
+      path: `items/${id}`,
+      params,
+    }
+  );
   return { data, error, isLoading };
 }
